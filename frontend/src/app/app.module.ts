2b66fc874ed2b1cd8b0b997b48bc2e24b974d220
Dalmiro18 <dalmiro2h@gmail.com>
2020-11-19 20:09:52 -0300

    boceteado y posiciones de la main page y el navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
